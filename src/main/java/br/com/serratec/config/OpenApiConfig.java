b6a66eb1f718a31184f1131a8540ef1bb82a41da
Suru13 <78802877+Suru13@users.noreply.github.com>
2024-10-18 23:19:56 -0300

    finalizado

--- src/main/java/br/com/serratec/config/OpenApiConfig.java
+++ src/main/java/br/com/serratec/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package br.com.serratec.config;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
@@ -19,6 +20,7 @@ public class OpenApiConfig {
 	@Value("${dominio.openapi.prod-url}")
 	private String prodUrl;
 
+	@Bean
 	public OpenAPI myOpenApi() {
 		Server devServer = new Server();
 		devServer.setUrl(devUrl);
@@ -29,9 +31,10 @@ public class OpenApiConfig {
 		prodServer.setDescription("URL da área de Produção");
 		
 		Contact contact = new Contact();
-		contact.setName("Roni Schanuel");
-		contact.setEmail("roni_inf@hotmail.com");
-		contact.setUrl("https://www.serratec.com.br");
+		contact.setName("Grupo7");
+		//contact.setEmail("");
+		contact.setUrl("https://github.com/Suru13/Vendedor_Api_atividade");
+		
 		
 		License apacheLicense = new License().name("Apache")
 								.url("https://www.apache.org/licenses/LICENSE-2.0");
@@ -39,6 +42,7 @@ public class OpenApiConfig {
 		Info info = new Info().title("API DO SERRATEC").version("1.0").contact(contact)
 				.description("API PARA ESTUDOS").termsOfService("https://www.serratec.com.br")
 				.license(apacheLicense);
+		
 		
 		return new OpenAPI().info(info).servers(List.of(devServer,prodServer));
 		
